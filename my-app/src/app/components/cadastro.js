bc62d475b425a49ba8a4f7f5ce1d00926e0531f6
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-08-01 10:12:04 -0300

    Estilo da tela de Login completo

---
+++ my-app/src/app/components/cadastro.js
@@ -0,0 +1,48 @@
+"use client"; // Isso garante que o Next.js trate este componente como um Client Component
+
+import { useState } from 'react';
+import styles from '../styles/Cadastro.module.css'; // Ajuste o caminho conforme necessário
+
+function Cadastro() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    // Função que será chamada quando o formulário for submetido
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const usuario = {
+            email,
+            senha
+        };
+
+        console.log('Dados do usuário:', usuario);
+
+        window.location.href = 'http://www.google.com.br';
+    }
+
+    return (
+        <div className={styles.container}>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <h1 className={styles.LOGIN}>LOGIN</h1>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className={styles.input}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                    className={styles.input}
+                />
+                <button type="submit" className={styles.button}>Entrar</button>
+            </form>
+        </div>
+    );
+}
+
+export default Cadastro;
