f7496bb2135812a2d25aafbef0c5cd01121fc08d
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-08-06 08:31:04 -0300

    chore: Convertendo arquivos para JSX e adicionando estilos da tela de Login

---
+++ src/pages/_app.jsx
@@ -0,0 +1,7 @@
+import "@/styles/globals.css";
+import Cadastro from "../pages/cadastro.jsx";
+
+export default function App() {
+  return < 
+  Cadastro />;
+}
---
+++ src/pages/cadastro.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from 'react'; 
+import styles from '../styles/cadastro.module.css';
+
+function Cadastro() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const usuario = {
+            email,
+            senha
+        };
+
+        console.log('Dados do usuário:', usuario);
+
+        window.location.href = 'http://www.google.com.br';
+    }
+
+    return (
+        <div className={styles.container}>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <h1 className={styles.LOGIN}>LOGIN</h1>
+            
+                <div className={styles.inputGroup}>
+                    <input
+                         type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className={styles.input}
+                    />
+                </div>
+                
+                <div className={styles.inputGroup}>
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                        className={styles.input}
+                    />
+                </div>
+                
+                <button type="submit" className={styles.button}>Entrar</button>
+            </form>
+        </div>
+    );
+}
+
+export default Cadastro;
