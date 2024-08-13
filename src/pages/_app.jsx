463cd5d346d77ec85203b4659ad5180e1120d415
M4ri4Edd <luzdudinha@gmail.com>
2024-08-13 17:19:25 -0300

    update header

---
+++ src/pages/_app.jsx
@@ -0,0 +1,8 @@
+import "@/styles/globals.css";
+ import Cadastro from "./cadastro.jsx";
+ import Login from "./login.jsx";
+ import Menu from './index.jsx';
+
+export default function App({ Component, pageProps }) {
+    return <Menu/>;
+}
--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -4,23 +4,22 @@ import styles from '../../styles/header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
- 
+        <h1 className={styles.logo}>
+            <Link href='/index'>ArmaZen</Link>
+        </h1>
         <nav>
             <ul className={styles.navList}>
-                <li className={styles.navItem}>
-                    <Link href="/">Home</Link>
-                </li>
                 <li className={styles.navItem}>
                     <Link href="/">Cadastro</Link>
                 </li>
                 <li className={styles.navItem}>
-                    <Link href="/about">Visualização</Link>
+                    <Link href="/">Visualização</Link>
                 </li>
                 <li className={styles.navItem}>
-                    <Link href="/contact">Movimentação</Link>
+                    <Link href="/">Movimentação</Link>
                 </li>
                 <li className={styles.navItem}>
-                    <Link href="/contact">Relatório</Link>
+                    <Link href="/">Relatório</Link>
                 </li>
             </ul>
         </nav>
