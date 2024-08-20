1d5e98fd2888c0cc9502ce1e2922f0777fcae69c
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-08-20 10:26:38 -0300

    Tela de cadastro de itens

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,10 @@
 import "@/styles/globals.css";
- import Cadastro from "./cadastro.jsx";
- import Login from "./login.jsx";
- import Menu from './index.jsx';
+import Cadastro from "./cadastro.jsx";
+import Login from "./login.jsx";
+import Menu from './index.jsx';
+import CadastroItens from './cadastroItens.jsx';
+
 
 export default function App({ Component, pageProps }) {
-    return <Menu/>;
+    return <CadastroItens />;
 }
---
+++ src/pages/cadastroItens.jsx
@@ -0,0 +1,64 @@
+"use client";
+import React from 'react';
+import Link from 'next/link';
+import Header from './components/header.jsx';
+import styles from '../styles/cadastroItens.module.css';
+import Footer from './components/footer.jsx';
+
+function CadastroItens() {
+    return (
+        <div>
+            <Header/>
+            <div>
+                <h2 className={styles.h2}>Cadastro de Itens</h2>
+            
+                <main className={styles.container}>
+                    <section className={styles.formSection}>
+                        <form>
+                            <div className={styles.formRow}>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="codigo">Código</label>
+                                    <input type="text" id="codigo" name="codigo" />
+                                </div>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="tipo-item">Tipo do Item</label>
+                                    <input type="text" id="tipo-item" name="tipo-item" />
+                                </div>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="fornecedor">Fornecedor</label>
+                                    <input type="text" id="fornecedor" name="fornecedor" placeholder="Opcional" />
+                                </div>
+                            </div>
+                            <div className={styles.formRow}>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="nome-item">Nome do item</label>
+                                    <input className={styles.nameItem} type="text" id="nome-item" name="nome-item" />
+                                </div>
+                            </div>
+                            <div className={styles.formRow}>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="quantidade">Quantidade</label>
+                                    <input type="text" id="quantidade" name="quantidade" />
+                                </div>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="unidade-medida">Unidade de Medida</label>
+                                    <input type="text" id="unidade-medida" placeholder="Caixa" />
+                                </div>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="validade">Data de Validade</label>
+                                    <input type="text" id="validade" name="validade" placeholder="Opcional" />
+                                </div>
+                            </div>
+                            <div className={styles.formRow2}>
+                                <button type="submit" className={styles.btn}>Cadastrar</button>
+                            </div>
+                        </form>
+                    </section>
+                </main>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default CadastroItens;
