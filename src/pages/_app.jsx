07094c20a617c09a629b48149e16bd1008e4494b
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-08-20 11:08:58 -0300

    Retirando tela de cadastro

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,9 @@
 import "@/styles/globals.css";
-import Cadastro from "./cadastro.jsx";
 import Login from "./login.jsx";
 import Menu from './index.jsx';
 import CadastroItens from './cadastroItens.jsx';
 
 
 export default function App({ Component, pageProps }) {
-    return <CadastroItens />;
+    return <Login />;
 }
