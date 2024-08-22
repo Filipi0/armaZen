fe43650f28cc7021ddde0ea1dfcbd2c8ea5acc7e
Maria Luz <135741080+M4ri4Edd@users.noreply.github.com>
2024-08-22 19:54:01 -0300

    ajustes

--- src/pages/components/footer.jsx
+++ src/pages/components/footer.jsx
@@ -4,7 +4,7 @@ import styles from '../../styles/footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-        <p>Desenvolvido por Filipi Dantas e Maria Luz</p>
+        <p className={styles.text}>Desenvolvido por Filipi Dantas e Maria Luz</p>
     </footer>
   );
 }
