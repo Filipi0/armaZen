7b1dcc6057725fcf38948eee227fce9ad9366414
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-09-03 13:05:08 -0300

    Tabela ficticia da tela de vizualizar usuários

--- src/pages/vizualizar-usuario.jsx
+++ src/pages/vizualizar-usuario.jsx
@@ -9,6 +9,14 @@ function VisualizarUsuarios() {
     console.log('Pesquisar usuário...');
   };
 
+  // Dados fictícios
+  const usuarios = [
+    { nome: 'João Silva', email: 'joao.silva@example.com', permissao: 'Administrador' },
+    { nome: 'Maria Oliveira', email: 'maria.oliveira@example.com', permissao: 'Usuário' },
+    { nome: 'Pedro Santos', email: 'pedro.santos@example.com', permissao: 'Usuário' },
+    { nome: 'Ana Costa', email: 'ana.costa@example.com', permissao: 'Administrador' },
+  ];
+
   return (
     <div>
       <Header />
@@ -19,7 +27,7 @@ function VisualizarUsuarios() {
             <div className={styles.searchContainer}>
               <label htmlFor="search" className={styles.searchLabel}>Pesquisar</label>
               <div className={styles.searchInputContainer}>
-                <input type="text" id="search" name="search"  />
+                <input type="text" id="search" name="search" placeholder="Digite o nome do usuário..." />
                 <button type="button" className={styles.searchIcon} onClick={handleSearch}>
                   <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24" fill="#00a7e1">
                     <path d="M796 1006 558 768q-33 28-74 42t-84 14q-130 0-220-90T90 514q0-130 90-220t220-90q130 0 220 90t90 220q0 44-14 84t-42 74l238 238-96 96ZM400 722q83 0 141.5-58.5T600 522q0-83-58.5-141.5T400 322q-83 0-141.5 58.5T200 522q0 83 58.5 141.5T400 722Z"/>
@@ -35,6 +43,26 @@ function VisualizarUsuarios() {
                 <option value="permissao">Permissão</option>
               </select>
             </div>
+          </section>
+
+          <section className={styles.headerSection}>
+            <div className={styles.headerItem}>Nome</div>
+            <div className={styles.headerItem}>Email</div>
+            <div className={styles.headerItem}>Permissão</div>
+          </section>
+
+          <section className={styles.tableSection}>
+            <table className={styles.userTable}>
+              <tbody>
+                {usuarios.map((usuario, index) => (
+                  <tr key={index}>
+                    <td>{usuario.nome}</td>
+                    <td>{usuario.email}</td>
+                    <td>{usuario.permissao}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </section>
         </main>
       </div>
