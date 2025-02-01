a4d44d57d82b63f77c6bb810c522237cf7c903d1
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-02-01 09:13:43 -0300

    refatora componentes e estilos, movendo CSS para diretório de componentes e adicionando funcionalidade de logout

--- src/pages/components/card.jsx
+++ src/pages/components/card.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from '../../styles/card.module.css';
+import styles from '../../styles/components/card.module.css';
 
 const Card = ({ titleCollapsed, count, items }) => {
   const [isExpanded, setIsExpanded] = useState(false);
--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -1,28 +1,49 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
-import styles from '../../styles/header.module.css';
+import Image from 'next/image';
+import styles from '../../styles/components/header.module.css';
 
 export default function Header() {
+  const router = useRouter(); // Hook para navegação
+
+  // Função para redirecionar ao clicar no logout
+  const handleLogout = () => {
+    router.push('/login'); // Redireciona para a página de login
+  };
+
   return (
     <header className={styles.header}>
+      <div className={styles['header-container']}>
+
+        {/* LOGO */}
         <h1 className={styles.logo}>
-            <Link href='/'>ArmaZen</Link>
+          <Link href='/'>ArmaZen</Link>
         </h1>
+
+        {/* NAVEGAÇÃO - Apenas para telas maiores */}
         <nav>
-            <ul className={styles.navList}>
-                <li className={styles.navItem}>
-                    <Link href="/cadastro-itens">Cadastro</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link href="/vizualizar-estoques">Visualização</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link href="/movimentar-estoque">Movimentação</Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link href="/relatorio">Relatório</Link>
-                </li>
-            </ul>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <Link href="/cadastro-itens">Cadastro</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/vizualizar-estoques">Visualização</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/movimentar-estoque">Movimentação</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/login">Log off</Link>
+            </li>
+          </ul>
         </nav>
+
+        {/* ÍCONE DE LOGOUT - Apenas para telas menores */}
+        <div className={styles['logout-icon']} onClick={handleLogout}>
+          <Image src="/logout.png" alt="Log off" width={30} height={30} />
+        </div>
+
+      </div>
     </header>
   );
 }
